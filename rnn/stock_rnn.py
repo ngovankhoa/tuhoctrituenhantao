4b84ec90f03f33fd8293907606a02f115b3ebf18
Ngo Van Khoa <ngovankhoa@gmail.com>
2023-10-29 14:05:56 +0700

    rnn

--- rnn/stock_rnn.py
+++ rnn/stock_rnn.py
@@ -1,21 +1,16 @@
+"""
+NeuralNet -- Dự đoán giá chứng khoán với RNN
+Copyright (C) 2023 Ngo Van Khoa.
+"""
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
-
-import tensorflow as tf
-from tensorflow import keras
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import SimpleRNN
-from keras.layers import Dropout
-from keras.layers import Input
 
 #nạp dữ liệu
 data = np.loadtxt('../data/apple.txt')
 
 #chuẩn hóa dữ liệu với MinMaxScaler
 scaler = MinMaxScaler()
-scaled_data = scaler.fit_transform(data.reshape(-1, 1)).flatten()
+data = scaler.fit_transform(data.reshape(-1, 1)).flatten()
 
 #tổ chức dữ liệu:
 #chúng ta sẽ chia dữ liệu huấn luyện là các mảng 60 
@@ -24,12 +19,11 @@ scaled_data = scaler.fit_transform(data.reshape(-1, 1)).flatten()
 time_len = 60
 X = []
 y = []
-for i in range(len(scaled_data) - time_len):
-    X.append(scaled_data[i:i + time_len])
-    y.append(scaled_data[i + time_len])
+for i in range(len(data) - time_len):
+    X.append(data[i:i + time_len])
+    y.append(data[i + time_len])
+print("X len:", len(X), "y len:", len(y))
 
-print("X:", len(X), "y:", len(y))
-   
 #tách tập dữ liệu test ra riêng tỉ lệ 80:20
 train_len = round(len(X) * 0.8)
 X_train = X[:train_len] 
@@ -42,30 +36,31 @@ X_train = np.asarray(X_train).reshape((-1, time_len, 1))
 y_train = np.asarray(y_train)
 X_test = np.asarray(X_test).reshape((-1, time_len, 1))
 y_test = np.asarray(y_test)
-
 print("X shape:", X_train.shape, "y shape:", y_train.shape)
 
-model = Sequential()
+#tạo mô hình
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Input, SimpleRNN, Dense
+model = Sequential([
+    Input(shape=(time_len, 1)),
+    SimpleRNN(128),
+    Dense(1)
+])
+model.compile(optimizer='adam', loss='mse')
 
-model.add(Input(shape=(time_len, 1)))
-model.add(SimpleRNN(128))
-model.add(Dense(1))
-model.compile(loss='mse', optimizer='adam', metrics=['mae'])
-
-
-history = model.fit(X_train, y_train, epochs = 50, batch_size =32)
-
+#huấn luyện và dự đoán giá dựa vào dữ liệu test
+model.fit(X_train, y_train, epochs = 50, batch_size =32)
 y_pred = model.predict(X_test)
-y_pred = scaler.inverse_transform(y_pred)
 
+#vẽ đồ thị so sánh
+import matplotlib.pyplot as plt
+y_pred = scaler.inverse_transform(y_pred)
 y_test = y_test.reshape((-1, 1))
 y_test = scaler.inverse_transform(y_test)
-
 y_test = np.reshape(y_test, (-1, 1))
 y_pred = np.reshape(y_pred,(-1, 1))
 plt.figure(figsize = (30,10))
-plt.plot(y_pred, ls = '--', label = 'Giá dự đoán', lw = 2)
-plt.plot(y_test, label = 'Giá thực tế')
+plt.plot(y_pred, ls='--', label='Giá dự đoán', lw = 2)
+plt.plot(y_test, label='Giá thực tế')
 plt.legend()
 plt.show()
-
